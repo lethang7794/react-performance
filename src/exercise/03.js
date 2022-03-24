1ddb7f8a08e2d661af633f562354a0f699fe21c2
Le Quang Thang <lethang7794@gmail.com>
2022-03-24 21:47:29 +0700

    Ex 3 - Extra 2: pass only primitive values

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -15,18 +15,22 @@ function Menu({
 }) {
   return (
     <ul {...getMenuProps()}>
-      {items.map((item, index) => (
-        <ListItem
-          key={item.id}
-          getItemProps={getItemProps}
-          item={item}
-          index={index}
-          selectedItem={selectedItem}
-          highlightedIndex={highlightedIndex}
-        >
-          {item.name}
-        </ListItem>
-      ))}
+      {items.map((item, index) => {
+        const isSelected = selectedItem?.id === item.id
+        const isHighlighted = highlightedIndex === index
+        return (
+          <ListItem
+            key={item.id}
+            getItemProps={getItemProps}
+            item={item}
+            index={index}
+            isSelected={isSelected}
+            isHighlighted={isHighlighted}
+          >
+            {item.name}
+          </ListItem>
+        )
+      })}
     </ul>
   )
 }
@@ -37,12 +41,10 @@ function ListItem({
   getItemProps,
   item,
   index,
-  selectedItem,
-  highlightedIndex,
+  isSelected,
+  isHighlighted,
   ...props
 }) {
-  const isSelected = selectedItem?.id === item.id
-  const isHighlighted = highlightedIndex === index
   return (
     <li
       {...getItemProps({
@@ -58,21 +60,7 @@ function ListItem({
   )
 }
 // 🐨 Memoize the ListItem here using React.memo
-ListItem = React.memo(ListItem, (prevProps, nextProps) => {
-  if (prevProps.getItemProps !== nextProps.getItemProps) return false
-  if (prevProps.index !== nextProps.index) return false
-  if (prevProps.item !== nextProps.item) return false
-  if (prevProps.selectedItem !== nextProps.selectedItem) return false
-
-  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
-    const wasLastHighlight = prevProps.highlightedIndex === prevProps.index
-    const isNowHighlight = nextProps.highlightedIndex === nextProps.index
-    if (wasLastHighlight || isNowHighlight) return false
-    return false
-  }
-
-  return true
-})
+ListItem = React.memo(ListItem)
 
 function App() {
   const forceRerender = useForceRerender()
